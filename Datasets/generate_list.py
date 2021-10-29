19413fb24f97d5ad0ba530a1c48e1680ab71ac59
minibeast <>
2021-10-29 16:27:03 +0200

    Try get heatmap

--- Datasets/generate_list.py
+++ Datasets/generate_list.py
@@ -1,16 +1,26 @@
 import os
 
 # Variables
-dateset = "kitti"  # name of the text file
-raw_dir = "/home/kevin/Project_Local/KITTI_validation_tests/depth_selection/test_depth_completion_anonymous/image"  # folder of the raw images
-gt_dir = "/home/kevin/Project_Local/KITTI_validation_tests/depth_selection/test_depth_completion_anonymous/velodyne_raw"
-seperator = "/"  # path seperator ('/' for linux, '\' for Windows)
+dataset = "not_kitti"  # name of the text file
+raw_dir = "../../Unstructured/eval/rgb"  # folder of the raw images
+gt_dir = "../../Unstructured/eval/gt"
 
-raw_pics = os.listdir(raw_dir)
-gt_pics = os.listdir(gt_dir)
-with open("{}.txt".format(dateset), 'w') as f:
-    if dateset == "kitti":
+raw_pics = sorted(os.listdir(raw_dir))
+gt_pics = sorted(os.listdir(gt_dir))
+if dataset == "kitti":
+    with open("{}.txt".format(dataset), 'w') as f:
         for i in range(len(raw_pics)):
             f.write("{}/{} {}/{} 721.533\n".format(raw_dir, raw_pics[i], gt_dir, gt_pics[i]))
-    else:
-        print("not kitti")
+            # use a "/" for linux and a "\" for windows
+        print("Done KITTI")
+else:
+    # with open("train_unstructured.txt", 'w') as f:
+    #     for i in range(len(raw_pics)):
+    #         f.write("{}/{} {}/{} 526.526\n".format(raw_dir, raw_pics[i], gt_dir, gt_pics[i]))
+    #     print("Done unstructured")
+    with open("../eval_uns.txt", 'w') as f:
+        for i in range(len(raw_pics)):
+            f.write("rgb/{} gt/{} 526.526\n".format(raw_pics[i], gt_pics[i]))
+        print("Done unstructured")
+
+
--- Evaluation/to_jet.py
+++ Evaluation/to_jet.py
@@ -1,17 +1,17 @@
-#Edited code originally by Samantha Ball
+# Edited code originally by Samantha Ball
 
 import matplotlib.pyplot as plt
 import os
 import matplotlib.image as mpimg
 
-folder = "/media/kevin/KP" #folder name containing 16-bit depth maps
+folder = "/home/minibeast/Kevin/bts/pytorch/result_bts_eigen_v2_pytorch_1_layer/raw"  # folder name containing 16-bit depth maps
 
-for filename in os.listdir(folder):
+for filename in sorted(os.listdir(folder)):
     print(filename)
-    if not filename.endswith((".png", "jpg")): #to ensure continuous processing
+    if not filename.endswith((".png", "jpg")):  # to ensure continuous processing
         continue
     img = mpimg.imread(os.path.join(folder, filename))
-    plt.imshow(img, cmap="jet") #plot as jet colour map
-
-    plt.axis('off') #remove axes
-    plt.savefig(filename.split(".")[0] + "_jet.png", bbox_inches='tight') #save without surrounding whitespace
+    plt.imshow(img, cmap="jet")  # plot as jet colour map
+    plt.axis('off')  # remove axes
+    plt.savefig("jet1/" + filename.split(".")[0] + "_jet.png",
+                bbox_inches='tight')  # save without surrounding whitespace
